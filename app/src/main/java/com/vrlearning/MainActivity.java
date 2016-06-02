a7c9d26ec0214ebcb3f4a688a8f377bd4d320462
Juraj <juraj.begovac@bellabeat.com>
2016-06-02 10:29:22 +0200

    Add library dependencies. Add sample video

--- app/src/main/java/com/vrlearning/MainActivity.java
+++ app/src/main/java/com/vrlearning/MainActivity.java
@@ -1,7 +1,11 @@
 package com.vrlearning;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        final PagerAdapter adapter = new FragmentStatePagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                switch (position) {
+                    case 0:
+                        return VRFragment.newInstance("video_4.mp4");
+                }
+                return null;
+            }
+            @Override
+            public int getCount() {
+                return 1;
+            }
+        };
+
+        assert viewPager != null;
+        viewPager.setAdapter(adapter);
     }
 }
